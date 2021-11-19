afd6413f680d795c3355cae9c375dca4fec14e08
UvinPerera <uvininduwaraperera@gmail.com>
2021-11-19 18:09:07 +0530

    created swagger documentation

--- pet-store/src/main/java/org/acme/PetResource.java
+++ pet-store/src/main/java/org/acme/PetResource.java
@@ -2,12 +2,19 @@ package org.acme;
 
 import org.acme.models.Pet;
 import org.acme.repository.PetRepository;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/pets")
+@Tag(name = "Pet API", description = "All the pet related CRUD operation APIs are here")
 public class PetResource {
     private final PetRepository petRepository;
 
@@ -17,6 +24,19 @@ public class PetResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary = "Get all pets",
+            description = "This endpoint returns all pets in the database"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "API returns all pets",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
     public Response get(){
         return Response.ok(petRepository.getAllPets()).build();
     }
@@ -24,13 +44,54 @@ public class PetResource {
     @GET
     @Path("/query")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getByColumn(@QueryParam("column") String column, @QueryParam("value") String value){
+    @Operation(
+            summary = "Get pets by query",
+            description = "This endpoint returns all pets in the database with a the column and the corresponding value provided"
+    )
+
+    @APIResponse(
+            responseCode = "200",
+            description = "API returns the filtered pets",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response getByColumn(
+            @Parameter(
+            description = "Column in the database",
+            required = true
+            )
+            @QueryParam("column") String column,
+            @Parameter(
+                    description = "The value need to be filter w.r.t column",
+                    required = true
+            )
+            @QueryParam("value") String value){
         return Response.ok(petRepository.getPetByColumnAndValue(column,value)).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response post(Pet pet){
+    @Operation(
+            summary = "Add a pet",
+            description = "This endpoint adds a pet in the database, Don't add an ID explicitly"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Pet created successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response post(
+            @RequestBody(
+                    required = true
+            )
+            Pet pet
+    ){
         try{
             petRepository.addPet(pet);
             return Response.status(201).build();
@@ -43,7 +104,23 @@ public class PetResource {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response put(Pet pet){
+    @Operation(
+            summary = "Update a pet",
+            description = "This endpoint updates a pet in the database, with an ID provided"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Pet updated successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response put(
+            @RequestBody(
+                    required = true
+            )
+                    Pet pet){
         try{
             petRepository.updatePet(pet);
             return Response.status(200).build();
@@ -55,7 +132,23 @@ public class PetResource {
     }
 
     @DELETE
-    public Response delete(@QueryParam("id") Long id){
+    @Operation(
+            summary = "Delete a pet",
+            description = "This endpoint delete a pet in the database, with an ID provided"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Pet deleted successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response delete(
+            @Parameter(
+                    required = true
+            )
+            @QueryParam("id") Long id){
         try{
             petRepository.deletePet(id);
             return Response.status(200).build();
--- pet-store/src/main/java/org/acme/PetTypeResource.java
+++ pet-store/src/main/java/org/acme/PetTypeResource.java
@@ -2,11 +2,19 @@ package org.acme;
 
 import org.acme.models.PetType;
 import org.acme.repository.PetTypeRepository;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 
 @Path("/pet-type")
+@Tag(name = "Pet type API", description = "CRUD operations for the pet-type related APIs are here")
 public class PetTypeResource {
   private final PetTypeRepository pettypeRepository;
 
@@ -17,13 +25,42 @@ public class PetTypeResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+	summary = "Get all pet-types",
+	description = "This endpoint returns all pet-types in the database"
+    )
+    @APIResponse(
+	responseCode = "200",
+	description = "API returns all pet types",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
     public Response get() {
         return Response.ok(pettypeRepository.getAllPetTypes()).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response post(PetType petType){
+    @Operation(
+            summary = "Add a pet type",
+            description = "This endpoint adds a pet-type in the database, Don't add an ID explicitly"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Pet type created successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response post(
+            @RequestBody(
+                    required = true
+            )
+                    PetType petType){
       try {
         pettypeRepository.addPetType(petType);
         return Response.status(201).build();
@@ -36,7 +73,23 @@ public class PetTypeResource {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response update(PetType petType){
+    @Operation(
+            summary = "Update a pet type",
+            description = "This endpoint updates a pet-type in the database, with an ID provided"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Pet type updated successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response update(
+            @RequestBody(
+                    required = true
+            )
+                    PetType petType){
       try{
         pettypeRepository.updatePetType(petType);
         return Response.status(200).build();
@@ -47,11 +100,26 @@ public class PetTypeResource {
       }
     }
 
-    /**
-     * @param id The Id of the Pet type
-     * */
+
     @DELETE
-    public Response delete(@QueryParam("id") Long id){
+    @Operation(
+            summary = "Delete a pet type",
+            description = "This endpoint delete a pet-type in the database, with an ID provided"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Pet type deleted successfully"
+    )
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Error"
+    )
+    public Response delete(
+            @Parameter(
+                    required = true
+            )
+            @QueryParam("id") Long id
+    ){
       try{
         pettypeRepository.deletePetType(id);
         return Response.status(200).build();
